f19c7b418c2852fd234e957d95496777b1bbaac5
sprucetonye <waribosamuel@gmail.com>
2022-10-31 16:17:43 +0100

    host my index page now

--- djangoapi_home/details/urls.py
+++ djangoapi_home/details/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
+    
     path('admin', admin.site.urls),
     path('profiles/', views.profile_list),
+    re_path(r'^$', views.profile_list, name='profiles'),
 ]
 
--- djangoapi_home/djangoapi/urls.py
+++ djangoapi_home/djangoapi/urls.py
@@ -1,8 +1,11 @@
 
+import re
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', include('details.urls')),
+    
+    
 ]
